44711912004ccf66526a46da1da149b764f68694
Leonardo Santos <leonardodossantoss50@gmail.com>
2024-12-08 21:55:03 -0300

    Criado os componentes da asidebar e logica para status da visualzação mobile.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
 
 function App() {
+  const myWidth = 200
   return (
-    <div>
-      <div>Minha aplicação</div>
+    <div className="App">
+      <Navbar
+        drawerWidth={myWidth}
+        Content={
+          <Routes>
+            <Route path="" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+          </Routes>
+        }
+        />
     </div>
   );
 }
